723912171cdd9c2c61b3bf1ffdd7b63e0599d36d
Hao <thienhaonguyen07@gmail.com>
2023-08-10 07:28:32 +0700

    new

---
+++ UI/src/app/model/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    productID:string;
+    productName:string;
+    categoryName:string;
+    price:number;
+    playerName:string;
+    teamName:string;
+    pictureUrl:string;
+    description:string;
+}
